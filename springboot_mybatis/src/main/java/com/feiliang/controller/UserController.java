bb0b4c99a91e951412bac6ad45dab184f1b9eaba
feiliang <feiliang93@126.com>
2018-10-26 14:25:48 +0800

    springboot

---
+++ springboot_mybatis/src/main/java/com/feiliang/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.feiliang.controller;
+
+import com.feiliang.model.User;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    public List<User> QueryUserList(){
+        return null;
+    }
+
+}
